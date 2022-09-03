8e77e87d41ddbc62639a633500afa6d6727b2806
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-03 13:39:21 +0530

    Time: 13 ms (85.86%), Space: 11.1 MB (23.60%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;        // element and index
+        int temp;
+        for(int i = 0 ; i< nums.size() ; i++){
+            temp = target - nums[i];        // finding compliment
+            if(mp.count(temp)){
+                return vector<int>{mp[temp] , i};   // returning the value of index and current index
+            }
+            mp.insert({nums[i] , i});
+        }
+        return vector<int>{};
+    }
+};
