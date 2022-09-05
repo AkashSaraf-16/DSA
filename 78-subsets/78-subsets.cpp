1767f3009f8f93917a1e79616363d48f231c7759
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-05 07:37:33 +0530

    Time: 3 ms (74.46%), Space: 7.4 MB (51.57%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,7 +3,6 @@ public:
     void helper(vector<int> nums, vector<vector<int>> &ans, vector<int> ds, int index){
         ans.push_back(ds);
         for(int i = index ; i < nums.size() ; i++){
-            if(i > index && nums[i] == nums[i-1]) continue;
             ds.push_back(nums[i]);
             helper(nums,ans,ds,i+1);
             ds.pop_back();
