253ec1a4d03e9a92d228a70158c2ddb8018c9e1f
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-08 08:04:01 +0530

    Time: 58 ms (11.92%), Space: 24 MB (87.04%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
+    // Single pass TC:O(N) SC:O(N)
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> fromLeft(n,1);
+     /*   vector<int> fromLeft(n,1);
         vector<int> fromRight(n,1);
         
         for(int i = 1 ; i < n ; i++){
@@ -13,6 +14,17 @@ public:
         vector<int> res(n);
         for(int i = 0 ; i<n ; i++){
             res[i] = fromLeft[i]*fromRight[n-1-i];
+        }
+        return res;
+        */
+    // Single pass TC:O(N) SC:O(N)
+        int fromLeft = 1, fromRight = 1;
+        vector<int> res(n,1);
+        for(int i=0; i<n ; i++){
+            res[i]*=fromLeft;
+            fromLeft*=nums[i];
+            res[n-1-i]*=fromRight;
+            fromRight*=nums[n-1-i];
         }
         return res;
     }
