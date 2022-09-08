b30ea69283c810f5516f92e28d75abb49bd900d3
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-08 07:43:30 +0530

    Time: 55 ms (16.22%), Space: 25.1 MB (29.50%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> lhs(nums.size(),1); // constant space
+        int n = nums.size();
+        vector<int> fromLeft(n,1);
+        vector<int> fromRight(n,1);
         
-        int next;       // to store the (i+1)th element while calculating the rhs product part
-        // calculating lhs array : an array which has product of all elements before that index
-        for(int i = 1 ; i < nums.size() ; i++){
-            lhs[i] = lhs[i-1]*nums[i-1]; 
-        }
-        next = nums.back();
-        nums.back() = 1;
-        // calculating rhs array : an array which has product of all elements after that index
-        for(int i = nums.size()-2 ; i>=0 ; i--){
-            int temp = nums[i];
-            nums[i] = nums[i+1]*next;
-            next = temp;
-        }
-        // nums[i] = lhs[i]*rhs[i](rhs[i] = nums[i] as I have implemented it in the same array)
-        for(int i = 0 ; i < nums.size() ; i++){
-            nums[i] *= lhs[i];
+        for(int i = 1 ; i < n ; i++){
+            fromLeft[i] = fromLeft[i-1]*nums[i-1];
+            fromRight[i] = fromRight[i-1]*nums[n-i];
         }
         
-        return nums;
-        
+        vector<int> res(n);
+        for(int i = 0 ; i<n ; i++){
+            res[i] = fromLeft[i]*fromRight[n-1-i];
+        }
+        return res;
     }
 };
