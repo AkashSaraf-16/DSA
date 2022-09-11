3c66ce757c7b94c0ba38cf58b2a2f5f73a01b1cf
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-11 06:08:51 +0530

    Time: 108 ms (84.81%), Space: 41.5 MB (48.90%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        // vector<int> v;
+        // while(temp){
+        //     v.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // vector<int> ans;
+        // stack<int> s;
+        // for(int i = v.size()-1 ; i>=0 ; i--){
+        //     while(!s.empty() && s.top()<=v[i])
+        //         s.pop();
+        //     if(s.empty())
+        //         ans.push_back(0);
+        //     else
+        //         ans.push_back(s.top());
+        //     s.push(v[i]);
+        //     }
+        // reverse(ans.begin(),ans.end());
+        // return ans;
+        
+        ListNode* temp = head;
+        vector<int> res;
+        stack<int> s;
+        for(auto n = head ; n != nullptr ; n = n->next) res.push_back(n->val);
+        for(int i = res.size()-1 ; i>=0 ; i--){
+            auto val = res[i];
+            while(!s.empty() && s.top() <= val)
+                s.pop();
+            res[i] = s.empty() ? 0 : s.top();
+            s.push(val);
+        }
+        return res;
+    }
+};
