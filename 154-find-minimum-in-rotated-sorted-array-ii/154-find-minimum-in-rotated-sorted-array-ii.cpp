43e221291a86ebcd2854299688554f90f896753b
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-25 20:04:32 +0530

    Time: 12 ms (41.04%), Space: 12.4 MB (18.66%) - LeetHub

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // TC:O(LOGN) IN AVG AND O(N) IN WORST CASE
+    int findMin(vector<int>& n) {
+        int s=0,l=n.size();
+        int e =l-1;
+        while(s<e){
+            int m = s+(e-s)/2;
+            if(n[m] > n[e]) s=m+1;          // right part is unsorted hence we can find min here
+            else if(n[s] > n[m]) e=m;       // right side has small value (not rotated)
+            else e--;                       // mid value equal to end value move towards start
+        }
+        return n[s];
+    }
+};
