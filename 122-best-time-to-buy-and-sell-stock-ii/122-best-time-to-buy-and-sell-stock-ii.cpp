657688814bc42d166f79af304b3dc97c23a6c6c1
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-09-27 07:28:35 +0530

    Time: 11 ms (66.11%), Space: 15.2 MB (24.44%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return helper(prices,0,true,dp);
-    }
-    int helper(vector<int> &prices , int ind , bool buy , vector<vector<int>> &dp){
-        if(ind == prices.size())
-            return 0;
-        if(dp[ind][buy]!=-1)
-            return dp[ind][buy];
-        
-        // Getting the recurrence relation
-        // exploring every possibilities
-        int profit = 0;
-        if(buy){                // if we can buy
-            profit = max((-prices[ind]) + helper(prices,ind+1,false,dp),helper(prices,ind+1,true,dp)); 
-                                            //      take(buy)               not take(not buy)
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        dp[n][0]=dp[n][1]=0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=1;buy>=0;buy--){
+                int profit = 0;
+                if(buy){                // if we can buy
+                    profit = max((-prices[ind]) + dp[ind+1][false],dp[ind+1][true]);
+                                                    //      take(buy)               not take(not buy)
+                }
+                else                    // if we can sell
+                    profit = max((prices[ind]) + dp[ind+1][true],dp[ind+1][false]); 
+                                                    //      sell                    // not sell
+                dp[ind][buy]=profit;                
+            }
         }
-        else                    // if we can sell
-            profit = max((prices[ind]) + helper(prices,ind+1,true,dp),helper(prices,ind+1,false,dp)); 
-                                            //      sell                    // not sell
-        return dp[ind][buy]=profit;
+        return dp[0][1];
     }
 };
