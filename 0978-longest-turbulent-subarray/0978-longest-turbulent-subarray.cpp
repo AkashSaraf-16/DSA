e5b324d93f14a5e1501c75230c9f61d857da9b2c
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-11-01 14:57:04 +0530

    Time: 103 ms (77.54%), Space: 40.1 MB (98.62%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int len=0;
-    void helper(vector<int>& arr,int prev ,int curr, bool incTrend , int l){
-        len=max(len,l);
-        
-        if(curr==arr.size())
-            return;
-        
-        if(incTrend==true && arr[prev]<arr[curr])         // if we want inc trend and it satifies
-            helper(arr,curr,curr+1,!incTrend,l+1);
-        else if(incTrend==false && arr[prev]>arr[curr])   // if we want dec trend and it satifies
-            helper(arr,curr,curr+1,!incTrend,l+1);
-        else{                                       // this can be due to two reasons
-            //1 when both the curr n the prev are equal
-            if(arr[curr]==arr[prev])
-                helper(arr,curr,curr+1,incTrend,1);
-                // helper(arr,curr,curr+1,!incTrend,1);   //we can do this also
-            //2 If we get two consecutive inc or dec elements
-            else
-            // eg: 2,4,6,1 ... here when curr=4 we wanted dip but we get 6 so we will reset our 
-            // series as 4,6 hence l=2 and since still we want dip int incTrend still holds
-                helper(arr,curr,curr+1,incTrend,2);
-        }
-    }
     int maxTurbulenceSize(vector<int>& arr) {
-        if(arr.size()==1)
-            return 1;
-        bool incTrend=true;
-        helper(arr,0,1,incTrend,1);
-        helper(arr,0,1,!incTrend,1);
-        return len;
+        int incsofar = 1;
+        int decsofar = 1;
+        int res = 1;
+        for(int i = 1; i < arr.size() ; i++){
+            if(arr[i-1]<arr[i]){        // if we encounter prev element lesser than current
+                decsofar = incsofar + 1;          // adding 1 in running length 
+                incsofar = 1;                // setting this becoz in next iteration greter no. ???
+            }
+            else if(arr[i-1]>arr[i]){   // if we encounter prev element greater than current
+                incsofar = decsofar + 1;          
+                decsofar = 1;
+            }
+            else{
+                incsofar = 1;
+                decsofar = 1;
+            }
+            res = max(res,max(incsofar,decsofar));
+        }
+        return res;
     }
 };
