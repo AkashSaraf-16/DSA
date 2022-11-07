215d15e673d7e64aa8db60dae9af8ee90cd152d7
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-11-07 19:13:41 +0530

    Time: 81 ms (31.73%), Space: 24.8 MB (93.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    void func1(vector<int>& nums,int k){
-        // TC:O(N) SC:O(N)
-        int n=nums.size();
-        vector<int> temp(n,0);
-        int j=0;
-        for(int i=n-k;i<n;i++){
-            temp[j]=nums[i];
-            j++;
+    void reverse(vector<int> &nums,int l, int r){
+        while(l<r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
         }
-        for(int i=0;i<n-k;i++){
-            temp[j]=nums[i];
-            j++;
-        }
-        nums=temp;
-    }
-    void rotateBy1(vector<int>& nums){
-        // TC:O(N) SC:O(1)
-        int n = nums.size();
-        int temp=nums[n-1];
-        for(int i=nums.size()-1;i>=1;i--){
-            nums[i]=nums[i-1];
-        }
-        nums[0]=temp;
     }
     void rotate(vector<int>& nums, int k) {
-        k=k%nums.size();
-        func1(nums,k);
-        // for(int i=1;i<=k%nums.size();i++)
-            // rotateBy1(nums);
+        int n=nums.size();
+        k=k%n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
     }
 };
