8892b554a46333c4841181d67c886c5a7517f8cd
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-11-07 19:42:11 +0530

    Time: 26 ms (87.79%), Space: 13.8 MB (69.28%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    // It is a classic Topological Sort problem where we can apply Kahn's Algorithm:
+    // Creating the adjency list
+    vector<int> adj[2001];
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> res;
+        // Step 1:Processing the input so that we can have an adjency list
+        for(auto edge : prerequisites){
+            int source = edge[1];
+            int dest = edge[0];
+            adj[source].push_back(dest);
+        }
+        // Step 2:Storing the indigree 
+        vector<int> indegree(numCourses,0);
+        for(int i=0 ; i<numCourses ;i++){
+            for(int v: adj[i]){
+                indegree[v]++;
+            }
+        }
+        // Step 3: Do a BFS traversal of the graph and inc the count whenever we process a vertex if at last we are unable to process all the vertex than there is some loop.
+        int count = 0;
+        queue<int> q;
+        // Pushing in queue(processing) all the 0 indigree vertices
+        for(int i = 0 ; i < numCourses ;i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        while(!q.empty()){
+            int u = q.front();
+            res.push_back(u);
+            q.pop();
+            for(int v:adj[u]){
+            // When we have processed the 'u' all the others 'v's dependencies will decrease by 1
+                --indegree[v];
+                if(indegree[v] == 0)
+                    q.push(v);
+            }
+            count++;
+        }
+        if(count==numCourses)
+            return res;
+        else 
+            return {};
+    }
+};
