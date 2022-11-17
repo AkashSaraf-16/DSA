734782c289b55207309cbaed9c5d97ffb9a23cb1
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-11-17 07:54:33 +0530

    Time: 539 ms (12.37%), Space: 99.7 MB (5.04%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<pair<int,int>> NSL,NSR;
+        stack<int> NSL,NSR;
         vector<int> indexL,indexR;
         
         for(int i = 0 ; i < heights.size() ; i++){
-            while(!NSL.empty() && NSL.top().first >= heights[i])
+            while(!NSL.empty() && heights[NSL.top()] >= heights[i])
                 NSL.pop();              // finding the next smallest element
             if(NSL.empty())
                 indexL.push_back(-1);   // if not found -1
             else
-                indexL.push_back(NSL.top().second);     // else index of that number
+                indexL.push_back(NSL.top());     // else index of that number
             
-            NSL.push({heights[i],i});       // pushing in stack current element and its index
+            NSL.push(i);       // pushing in stack current element and its index
         }
         for(int i = heights.size()-1 ; i >= 0 ; i--){
-            while(!NSR.empty() && NSR.top().first >= heights[i])
+            while(!NSR.empty() && heights[NSR.top()] >= heights[i])
                 NSR.pop();              // finding the next smallest element
             if(NSR.empty())
                 indexR.push_back(heights.size());   // if not found -1
             else
-                indexR.push_back(NSR.top().second);     // else index of that number
+                indexR.push_back(NSR.top());     // else index of that number
             
-            NSR.push({heights[i],i});       // pushing in stack current element and its index
+            NSR.push(i);       // pushing in stack current element and its index
         }
         reverse(indexR.begin() , indexR.end());     // reversing ths becoz we have filled it from right to left
         // indexR[indexR.size()-1] = indexR.size();    // next samllest to corner element will be this only
