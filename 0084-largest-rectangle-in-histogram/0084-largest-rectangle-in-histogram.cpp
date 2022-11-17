d786c25ce17c1cbdfaa016860da9dac2126790c0
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2022-11-17 06:24:11 +0530

    Time: 491 ms (22.72%), Space: 100.1 MB (5.04%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<pair<int,int>> NSL,NSR;
+        vector<int> indexL,indexR;
+        
+        for(int i = 0 ; i < heights.size() ; i++){
+            while(!NSL.empty() && NSL.top().first >= heights[i])
+                NSL.pop();              // finding the next smallest element
+            if(NSL.empty())
+                indexL.push_back(-1);   // if not found -1
+            else
+                indexL.push_back(NSL.top().second);     // else index of that number
+            
+            NSL.push({heights[i],i});       // pushing in stack current element and its index
+        }
+        for(int i = heights.size()-1 ; i >= 0 ; i--){
+            while(!NSR.empty() && NSR.top().first >= heights[i])
+                NSR.pop();              // finding the next smallest element
+            if(NSR.empty())
+                indexR.push_back(heights.size());   // if not found -1
+            else
+                indexR.push_back(NSR.top().second);     // else index of that number
+            
+            NSR.push({heights[i],i});       // pushing in stack current element and its index
+        }
+        reverse(indexR.begin() , indexR.end());     // reversing ths becoz we have filled it from right to left
+        // indexR[indexR.size()-1] = indexR.size();    // next samllest to corner element will be this only
+        vector<int> width;
+        for(auto i =0 ; i< indexR.size() ; i++)
+            width.push_back(indexR[i] - indexL[i] - 1) ;    // getting the width
+        int maxArea = INT_MIN;
+        for(int i = 0 ; i < heights.size() ; i++){
+            maxArea = max(maxArea,width[i]*heights[i]);
+        }
+        return maxArea;
+    }
+};
