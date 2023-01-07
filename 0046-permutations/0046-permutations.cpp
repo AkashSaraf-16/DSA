f85fdda911ee775eb56af2a1b11158a53919cba1
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-01-07 17:14:27 +0530

    Time: 4 ms (63.02%), Space: 8.1 MB (21.63%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    void helper(vector<int> &nums,vector<vector<int>> &res, int idx){
+    void helper(vector<int> nums,vector<vector<int>> &res, int idx){
         if(idx==nums.size()){
             res.push_back(nums);
             return;
         }
-        // idx here represents the index whose value we want to fix at present
         for(int i=idx;i<nums.size();i++){
-            swap(nums[i],nums[idx]);
-            helper(nums,res,idx+1);
-            swap(nums[i],nums[idx]);
+            if(i==idx || nums[i]!=nums[idx]){
+                swap(nums[i],nums[idx]);
+                helper(nums,res,idx+1);
+            }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
         helper(nums,res,0);
         return res;
     }
