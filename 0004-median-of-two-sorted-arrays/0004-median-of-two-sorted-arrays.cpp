a39aa3d73844f3c915d031b68725564c790d612a
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-01-17 09:02:52 +0530

    Time: 53 ms (53.98%), Space: 89.4 MB (72.34%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
+    // Naive Approach
+    // TC:O((m+n)/2) SC:O(1)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    if (nums1.size() > nums2.size()) {
-        swap(nums1, nums2);
-    }
-    int m = nums1.size(), n = nums2.size();
-    int imin = 0, imax = m, half_len = (m + n + 1) / 2;
-    while (imin <= imax) {
-        int i = (imin + imax) / 2;
-        int j = half_len - i;
-        if (i < m && nums2[j - 1] > nums1[i]) {
-            imin = i + 1;
-        } else if (i > 0 && nums1[i - 1] > nums2[j]) {
-            imax = i - 1;
-        } else {
-            int max_of_left;
-            if (i == 0) {
-                max_of_left = nums2[j - 1];
-            } else if (j == 0) {
-                max_of_left = nums1[i - 1];
-            } else {
-                max_of_left = max(nums1[i - 1], nums2[j - 1]);
+        int m=nums1.size(),n=nums2.size();
+        int mid=((m+n)/2)+1;
+        int i=0,j=0;
+        int curr=0,prev;
+        for(int x=0;x<mid;x++){
+            if(i==m){
+                prev=curr;
+                curr=nums2[j];
+                j++;
             }
-            if ((m + n) % 2 == 1) {
-                return max_of_left;
+            else if(j==n){
+                prev=curr;
+                curr=nums1[i];
+                i++;                
             }
-            int min_of_right;
-            if (i == m) {
-                min_of_right = nums2[j];
-            } else if (j == n) {
-                min_of_right = nums1[i];
-            } else {
-                min_of_right = min(nums1[i], nums2[j]);
+            else if(nums1[i]<nums2[j]){
+                prev=curr;
+                curr=nums1[i];
+                i++;                
             }
-            return (max_of_left + min_of_right) / 2.0;
+            else{
+                prev=curr;
+                curr=nums2[j];
+                j++;
+            }
         }
-    }
-    return 0.0;
-
+        if((m+n)%2)
+            return curr;
+        return 
+            (prev+curr)/2.0;
     }
 };
