14e1ed1ac865d821741d5123229723148f2bb174
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-01-18 07:22:00 +0530

    Time: 43 ms (76.01%), Space: 89.4 MB (72.27%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    // Naive Approach
-    // TC:O((m+n)/2) SC:O(1)
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size(),n=nums2.size();
-        int mid=((m+n)/2)+1;
-        int i=0,j=0;
-        int curr=0,prev;
-        for(int x=0;x<mid;x++){
-            if(i==m){
-                prev=curr;
-                curr=nums2[j];
-                j++;
+        if(nums2.size()<nums1.size())   return findMedianSortedArrays(nums2,nums1);
+        int m=nums1.size();
+        int n=nums2.size();
+        int l=0,h=m;
+        while(l<=h){
+            int cut1=(l+h)/2;       // partition for nums1 array
+            int cut2=(n+m+1)/2-cut1;    // partition for nums2 array
+            int l1= cut1==0?INT_MIN:nums1[cut1-1];
+            int l2= cut2==0?INT_MIN:nums2[cut2-1];
+            int r1= cut1==m?INT_MAX:nums1[cut1];
+            int r2= cut2==n?INT_MAX:nums2[cut2];
+            
+            if(l1<=r2 && l2<=r1){
+                // if we got the right partition
+                if((m+n)%2==0)      // even
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+                else                // odd
+                    return max(l1,l2);
             }
-            else if(j==n){
-                prev=curr;
-                curr=nums1[i];
-                i++;                
-            }
-            else if(nums1[i]<nums2[j]){
-                prev=curr;
-                curr=nums1[i];
-                i++;                
+            else if(l1>r2){
+                h=cut1-1;
             }
             else{
-                prev=curr;
-                curr=nums2[j];
-                j++;
+                l=cut1+1;
             }
         }
-        if((m+n)%2)
-            return curr;
-        return 
-            (prev+curr)/2.0;
+        return 0.0;
     }
 };
