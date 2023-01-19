ddc9bc0b3947a757780084a60b43cf91b5c3e2eb
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-01-19 08:40:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (6.04%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long int s = 0 , e = x,mid,temp;
-        while(s<=e){
-            mid = (s+e)/2;
-            if(mid*mid == x){
-                return mid;
+        long int l=0,h=x,temp;
+        while(l<=h){
+            long int m=(h-l)/2+l;
+            if(m*m==x)
+                return m;
+            else if(m*m<x){
+                temp=m;
+                l=m+1;
             }
-            else if(mid*mid < x){
-                s = mid+1;
-                temp = mid; // taking temp to track the nearest lowest value
-            }
-            else {
-                e = mid-1;;
-            }
+            else
+                h=m-1;
         }
-            return temp;
+        return temp;
     }
 };
