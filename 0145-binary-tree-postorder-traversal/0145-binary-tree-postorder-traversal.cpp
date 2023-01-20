96c091ae32971c19d6c7e747505f84e26ec8b001
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-01-20 07:00:34 +0530

    Time: 3 ms (55.30%), Space: 8.5 MB (44.69%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,19 +12,17 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        if(!root)
-            return {};
         stack<TreeNode*> st1,st2;
-        // We are using two stack to keep track of root nodes of subtrees and for left and right subtree processing and one for stroing all the nodes in postorder sequence
         vector<int> res;
-        TreeNode* curr=root;
-        st1.push(curr);
+        if(!root)
+            return res;
+        st1.push(root);
         while(!st1.empty()){
-            curr=st1.top();
+            TreeNode* curr=st1.top();
             st1.pop();
             st2.push(curr);
-            if(curr->left)      st1.push(curr->left);
-            if(curr->right)      st1.push(curr->right);
+            if(curr->left)  st1.push(curr->left);
+            if(curr->right)  st1.push(curr->right);
         }
         while(!st2.empty()){
             res.push_back(st2.top()->val);
