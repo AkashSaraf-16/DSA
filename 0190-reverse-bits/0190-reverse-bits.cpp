827c0648a17ff03a9cf4b7629a27cadce0e2a389
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-01-21 13:21:17 +0530

    Time: 3 ms (65.57%), Space: 6 MB (69.10%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -3,6 +3,7 @@ public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t res=0;
         for(int i=0;i<32;i++){
+            // left shifting it because in the next itertaion we have to consider the next bit
             res<<=1;
             if(n&1)
                 res++;
