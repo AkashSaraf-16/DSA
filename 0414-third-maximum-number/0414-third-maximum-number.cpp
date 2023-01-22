ee06789885b1f498615dfaa0fea20d5c6c128d12
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-01-22 09:28:24 +0530

    Time: 7 ms (76.44%), Space: 9.4 MB (42.54%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),greater<int>());
-        int cnt=1,prevEle=nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=prevEle){
-                cnt++;
-                prevEle=nums[i];
+        priority_queue<int,vector<int>,greater<int>> minHeap;
+        unordered_set<int> st;
+        for(int num:nums){
+            // If we have already taken the element
+            if(st.count(num))
+                continue;
+            if(minHeap.size()==3){
+                if(minHeap.top()<num){
+                    st.erase(minHeap.top());
+                    minHeap.pop();
+                    minHeap.push(num);
+                    st.insert(num);
+                }
             }
-            if(cnt==3){
-                return nums[i];
+            else{
+                st.insert(num);
+                minHeap.push(num);
             }
         }
-        return nums[0];
+        if(minHeap.size()==1)
+            return minHeap.top();
+        else if(minHeap.size()==2){
+            int f=minHeap.top();
+            minHeap.pop();
+            return max(f,minHeap.top());
+        }
+        return minHeap.top();
     }
 };
