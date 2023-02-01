ff68ccc6ee6417d27932b5231794492cc95a6c23
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-02-01 08:26:35 +0530

    Time: 228 ms (54.14%), Space: 91.4 MB (23.26%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> NSL,NSR;
-        vector<int> indexL,indexR;
+        stack<int> st;      // stores index
+        vector<int> leftSmaller,rightSmaller;
+        for(int i=0;i<heights.size();i++){
+            while(!st.empty() && heights[st.top()]>=heights[i])
+                st.pop();
+            if(st.empty())
+                leftSmaller.push_back(-1);
+            else
+                leftSmaller.push_back(st.top());
+            st.push(i);
+        }
+        while(!st.empty())
+            st.pop();
+        for(int i=heights.size()-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i])
+                st.pop();
+            if(st.empty())
+                rightSmaller.push_back(heights.size());
+            else
+                rightSmaller.push_back(st.top());
+            st.push(i);
+        }
+        reverse(rightSmaller.begin(),rightSmaller.end());
+        int area=0,tempArea=0;
+        for(int i=0;i<heights.size();i++){
+            tempArea=(rightSmaller[i]-leftSmaller[i]-1)*(heights[i]);
+            area=max(tempArea,area);
+        }
+        return area;
         
-        for(int i = 0 ; i < heights.size() ; i++){
-            while(!NSL.empty() && heights[NSL.top()] >= heights[i])
-                NSL.pop();              // finding the next smallest element
-            if(NSL.empty())
-                indexL.push_back(-1);   // if not found -1
-            else
-                indexL.push_back(NSL.top());     // else index of that number
-            
-            NSL.push(i);       // pushing in stack current element and its index
-        }
-        for(int i = heights.size()-1 ; i >= 0 ; i--){
-            while(!NSR.empty() && heights[NSR.top()] >= heights[i])
-                NSR.pop();              // finding the next smallest element
-            if(NSR.empty())
-                indexR.push_back(heights.size());   // if not found -1
-            else
-                indexR.push_back(NSR.top());     // else index of that number
-            
-            NSR.push(i);       // pushing in stack current element and its index
-        }
-        reverse(indexR.begin() , indexR.end());     // reversing ths becoz we have filled it from right to left
-        // indexR[indexR.size()-1] = indexR.size();    // next samllest to corner element will be this only
-        vector<int> width;
-        for(auto i =0 ; i< indexR.size() ; i++)
-            width.push_back(indexR[i] - indexL[i] - 1) ;    // getting the width
-        int maxArea = INT_MIN;
-        for(int i = 0 ; i < heights.size() ; i++){
-            maxArea = max(maxArea,width[i]*heights[i]);
-        }
-        return maxArea;
     }
 };
