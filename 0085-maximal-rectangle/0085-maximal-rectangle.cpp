d9345eb987e492a539a58bfd615177bc5d48e886
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-02-01 08:45:29 +0530

    Time: 67 ms (42.70%), Space: 17.6 MB (20.83%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,63 +1,48 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        int oneD[n];            
-        
-        vector<vector<int>> matrix2(m,vector<int>(n));
-        for(int i = 0 ; i < m ; i++)
-            for(int j = 0 ; j < n ; j++)
-                matrix2[i][j] = matrix[i][j] - '0';     // convert char i/p -> int
-            
-        for(int i = 0 ; i<n ; i++)
-            oneD[i] = matrix2[0][i];                    // convert 2D matrix -> 1D array
-        
-        int ans = MHA(oneD,n);                          // and applpying Max area of rectangle
-        for(int i = 1 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                oneD[j] = matrix2[i][j]==0?0:oneD[j]+1;  // adding in the height of previous height 
-            }
-            ans = max(ans,MHA(oneD,n));                 // max of the previous and this 1D area
+        int maxArea=0;
+        vector<int> h(matrix[0].size());
+        for(int i=0;i<matrix[0].size();i++)
+            h[i]=matrix[0][i]=='0'?0:1;
+        maxArea=largestRectangleArea(h);
+        for(int i=1;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++)
+                h[j]=matrix[i][j]=='0'?0:1+h[j];
+            maxArea=max(maxArea,largestRectangleArea(h));
         }
-        return ans;
-    }
-    
-    int MHA(int v[], int n){
-        stack<pair<int,int>> NSL,NSR;           //NSL: next smallest left NSR: next smallest right
-        int left[n], right[n];
-        
-        for(int i = 0 ; i < n ; i++){           // filling the left[]
-            while(!NSL.empty() && NSL.top().first >= v[i])
-                NSL.pop();
-            if(NSL.empty())
-                left[i] = -1;
-            else
-                left[i] = NSL.top().second;
-            
-            NSL.push({v[i], i});
-        }
-        
-        for(int i = n-1 ; i >= 0 ; i--){        // filling the right[]
-            while(!NSR.empty() && NSR.top().first >= v[i])
-                NSR.pop();
-            if(NSR.empty())
-                right[i] = n;
-            else
-                right[i] = NSR.top().second;
-            
-            NSR.push({v[i], i});
-        }
-        
-        for(int i = 0 ; i < n ; i++)            // calculating width
-            left[i] = right[i] - left[i] - 1;
-        
-        int maxArea = INT_MIN;
-        for(int i = 0 ; i < n ; i++)            // calculating the area
-            maxArea = max(maxArea,left[i]*v[i]);
-        
         return maxArea;
+    }
+        int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;      // stores index
+        vector<int> leftSmaller,rightSmaller;
+        for(int i=0;i<heights.size();i++){
+            while(!st.empty() && heights[st.top()]>=heights[i])
+                st.pop();
+            if(st.empty())
+                leftSmaller.push_back(-1);
+            else
+                leftSmaller.push_back(st.top());
+            st.push(i);
+        }
+        while(!st.empty())
+            st.pop();
+        for(int i=heights.size()-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i])
+                st.pop();
+            if(st.empty())
+                rightSmaller.push_back(heights.size());
+            else
+                rightSmaller.push_back(st.top());
+            st.push(i);
+        }
+        reverse(rightSmaller.begin(),rightSmaller.end());
+        int area=0,tempArea=0;
+        for(int i=0;i<heights.size();i++){
+            tempArea=(rightSmaller[i]-leftSmaller[i]-1)*(heights[i]);
+            area=max(tempArea,area);
+        }
+        return area;
         
     }
 };
