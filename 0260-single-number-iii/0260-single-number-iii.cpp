b7e5763fa391a2f81e5f38687015d2c1fe921a25
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-02-02 07:29:36 +0530

    Time: 9 ms (81.30%), Space: 10.1 MB (57.85%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int xoR = 0,n = nums.size();
+        for(int i = 0;i<n;i++) {
+            xoR ^= nums[i];
+        }
+        
+        int mask = 1;
+        while(1) {
+            if((mask & xoR) == 0) {         // tryig to find the rightmost set bit
+                mask = mask << 1;           // if not found shifting the bits 1 by left
+            }
+            else break;
+        }
+        
+        
+        int a = 0, b = 0;
+        for(int i = 0;i<n;i++) {
+            if(nums[i] & mask) a ^= nums[i];
+            else b ^= nums[i];
+        }
+        return {a, b}; 
+    }
+};
