4ddf5985024575c3a7dcac4aa9ba2770d74e955f
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-02-07 07:57:57 +0530

    Time: 47 ms (56.76%), Space: 18.6 MB (24.19%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,18 +11,35 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        // ListNode* curr=head;
+        // while(curr && curr->next && curr->next->next){
+        //     ListNode* end1=head;
+        //     while(end1->next->next){
+        //         end1=end1->next;
+        //     }
+        //     ListNode* end2=end1->next;
+        //     end1->next=NULL;
+        //     end2->next=curr->next;
+        //     curr->next=end2;
+        //     curr=curr->next->next;
+        // }
         stack<ListNode*> s;
         ListNode* curr=head;
-        while(curr && curr->next && curr->next->next){
-            ListNode* end1=head;
-            while(end1->next->next){
-                end1=end1->next;
-            }
-            ListNode* end2=end1->next;
-            end1->next=NULL;
-            end2->next=curr->next;
-            curr->next=end2;
+        int count=0;
+        while(curr){
+            count++;
+            s.push(curr);
+            curr=curr->next;
+        }
+        curr=head;
+        ListNode* end;
+        for(int i=0;i<count/2;i++){
+            end=s.top();
+            s.pop();
+            end->next=curr->next;
+            curr->next=end;
             curr=curr->next->next;
         }
+        curr->next=NULL;
     }
 };
