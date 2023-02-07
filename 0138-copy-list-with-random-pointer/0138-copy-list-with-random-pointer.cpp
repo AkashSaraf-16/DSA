8061f71d55331ce9a3c9fe3253729e573e9da470
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-02-07 07:26:29 +0530

    Time: 8 ms (74.39%), Space: 11.3 MB (29.79%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,24 +17,54 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*> mp;  // orginal-new
-        Node* curr=head;
-        // Creating single nodes from the Original LL
-        while(curr){
-            Node* copy=new Node(curr->val);
-            mp[curr]=copy;
-            curr=curr->next;
-        }
-        // Making the connection
-        curr=head;
-        while(curr){
-            Node* n=mp[curr->next];
-            Node* r=mp[curr->random];
-            mp[curr]->next=n;
-            mp[curr]->random=r;
-            curr=curr->next;
-        }
-        return mp[head];
+        // unordered_map<Node*,Node*> mp;  // orginal-new
+        // Node* curr=head;
+        // // Creating single nodes from the Original LL
+        // while(curr){
+        //     Node* copy=new Node(curr->val);
+        //     mp[curr]=copy;
+        //     curr=curr->next;
+        // }
+        // // Making the connection
+        // curr=head;
+        // while(curr){
+        //     Node* n=mp[curr->next];
+        //     Node* r=mp[curr->random];
+        //     mp[curr]->next=n;
+        //     mp[curr]->random=r;
+        //     curr=curr->next;
+        // }
+        // return mp[head];
         
+        
+        // Optimised solution:-
+        
+        // Inserting new nodes in Original LL
+        Node* curr=head;
+        while(curr){
+            Node *frnt=curr->next;
+            Node *copy=new Node(curr->val);
+            curr->next=copy;
+            copy->next=frnt;
+            curr=curr->next->next;
+        }
+        curr=head;
+        // Set the random of new LL
+        while(curr){
+            curr->next->random=curr->random == NULL ?NULL:curr->random->next;
+            curr=curr->next->next;
+        }
+        // Set the next pointers in LL and ditach the two LLs
+        curr=head;
+        Node* res=new Node(0);
+        Node* copy=res;
+        while(curr){
+            Node* frnt=curr->next->next;
+            copy->next=curr->next;
+            curr->next=frnt;
+            copy=copy->next;
+            curr=curr->next;
+        }
+        return res->next;
     }
 };
