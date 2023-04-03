2f9ce638e1c2d818784b94fa19470649ce80e79a
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-04-03 06:19:15 +0530

    Time: 4 ms (71.58%), Space: 7.3 MB (47.15%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-//     DP: dp array's ith index represents the max length of valid parenthesis at index i.
+//     TC:O(N)
+//     SC:O(N)
     int longestValidParentheses(string s) {
-        int maxLen=0;
-        vector<int> dp(s.length(),0);
-        for(int i=1;i<s.length();i++){
-            if(s[i]==')' && s[i-1]=='(')    // expression like  ....()
-                dp[i]=(i>=2?dp[i-2]:0)+2;
-            else if(i-dp[i-1]>0 && s[i]==')' && s[i-1]==')' && s[i-dp[i-1]-1]=='(')
-//          if we have expression like this : )(()) try to pput the values in above expression you will get it.
-                dp[i]=dp[i-1]+2+((i-dp[i-1])>=2?dp[i-dp[i-1]-2]:0);
-//          this last expression dp[i-dp[i-1]-2] can be understood from debugging expr like this : )()(()(()))
-            maxLen=max(maxLen,dp[i]);
+        stack<int> st;
+        st.push(-1);
+        int i=0,maxLen=0;
+        while(i<s.length()){
+            if(s[i]=='(')
+                st.push(i);
+            else{
+                st.pop();
+                if(st.empty())
+                    st.push(i);
+                else
+                    maxLen=max(maxLen,i-st.top());
+            }
+            i++;
         }
         return maxLen;
     }
