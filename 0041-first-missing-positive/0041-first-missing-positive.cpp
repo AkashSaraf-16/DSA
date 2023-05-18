895c354061de966d5c4e3369cbf19b919340577e
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-05-18 12:44:36 +0530

    Time: 290 ms (5.22%), Space: 65.5 MB (9.11%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        map<int,bool> map;
+        for(int i=1;i<=n;i++)
+            map.insert({i,false});
+        
+        for(auto i:nums){
+            if(i>0 && map.count(i))
+                map[i]=true;
+        }
+        
+        for(auto a:map){
+            if(a.second==false)
+                return a.first;
+        }
+        return n+1;
+    }
+};
