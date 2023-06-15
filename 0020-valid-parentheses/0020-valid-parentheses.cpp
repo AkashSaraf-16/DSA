a7146c545cd0e5b5e0986a42fca4ca24408f5a16
AkashSaraf-16 <81419782+AkashSaraf-16@users.noreply.github.com>
2023-06-15 08:38:07 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (97.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         int top=-1;
-        for(int curr=0;curr<s.length();curr++){
+        for(int curr=0;curr<s.size();curr++){
             if(top<0 || !isComplementery(s[top],s[curr])){
                 top++;
                 s[top]=s[curr];
@@ -12,9 +12,9 @@ public:
         }
         return top==-1;
     }
-    bool isComplementery(char a,char b){
-        if(a=='(' && b==')' || a=='{' && b=='}' || a=='[' && b==']')
+    bool isComplementery(char a, char b){
+        if((a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']'))
             return true;
-        return false; 
+        return false;
     }
 };
